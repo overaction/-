94ef609721787b82e26bd8d3ef2910ffd78829fa
mckim <kmc04@naver.com>
2021-07-25 01:20:40 +0900

    #3 주사위 굴리기 이벤트

--- 주사위게임/my/script.js
+++ 주사위게임/my/script.js
@@ -27,4 +27,39 @@ let currentScore = 0;
 let currentPlayer = 1;
 let lastScore = [0,0];
 
+// 플레이어 변경 function
+const changePlayer = (player) => {
+    if(player == 1) {
+        player1El.classList.remove('player-active');
+        player2El.classList.add('player-active');
+        currentScore1El.textContent = 0;
+        currentPlayer = 2;
+    }
+    else {
+        player2El.classList.remove('player-active');
+        player1El.classList.add('player-active');
+        currentScore2El.textContent = 0;
+        currentPlayer = 1;
+    }
+}
+
+// 주사위 굴리기 event
+rollBtn.addEventListener('click', () => {
+    // 1. 숫자 생성
+    const diceNum = Math.trunc(Math.random() * 6)+1;
+    // 2. 숫자에 맞는 주사위이미지 생성
+    diceEl.classList.remove('hidden');
+    diceEl.src = `./dice_img/dice-${diceNum}.png`
+    console.log(diceNum);
+    // 3. 숫자가 1일 경우 플레이어 체인지
+    // 아닐 경우 점수 추가
+    if(diceNum !== 1) {
+        currentScore += diceNum;
+        currentPlayer === 1 ? currentScore1El.textContent = currentScore : currentScore2El.textContent = currentScore 
+    }
+    else {
+        currentScore = 0;
+        currentPlayer === 1 ? changePlayer(1) : changePlayer(2);
+    }
+})
 
